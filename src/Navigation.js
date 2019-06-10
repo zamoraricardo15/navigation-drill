82a9cb22968a384f80c33504ebaf9d53824b8031
Alfredo Salazar <>
2019-06-10 13:24:01 -0500

    First commit

---
+++ src/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navigation.css';
+
+export default function Navigation(){
+    return (
+        <div>
+            <ul>
+                <li>Thinkful</li>
+                <li>Bloc</li>
+                <li>Hack Reactor</li>
+            </ul>
+        </div>
+    )
+}
